e475a04720e227d2bd91133d098347c5f0e7cf50
AnnijaEstereSkvarnavica <125013713+AnnijaEstereSkvarnavica@users.noreply.github.com>
2023-02-18 19:36:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
+    for i, next in enumerate(text): #enumerate atdod gan kārtas numuru gan simbolu 
         if next in "([{":
+            #Struktūrai jāpievieno vērtība 
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i + 1))
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+             
+                print("ir tukšs")
+                return i + 1 
             pass
 
 
@@ -25,6 +30,7 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    
 
 
 if __name__ == "__main__":
